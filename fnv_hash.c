0acdee5dda8ecb39e1d5bee61ebef5b4a1373a59
Eric Anholt <eric@anholt.net>
2009-11-23 11:35:52 +0100

    Add testing infrastructure.

---
+++ fnv_hash.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2009 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Eric Anholt <eric@anholt.net>
+ */
+
+/* Quick FNV-1 hash implementation based on:
+ * http://www.isthe.com/chongo/tech/comp/fnv/
+ *
+ * FNV-1 may not be the best hash out there -- Jenkins's lookup3 is supposed
+ * to be quite good, and it may beat FNV.  But FNV has the advantage that it
+ * involves almost no code.
+ */
+
+#include <string.h>
+#include "fnv_hash.h"
+
+uint32_t
+fnv1_hash_string(const void *key)
+{
+	uint32_t hash = 0;
+	const unsigned char *str = key;
+
+	while (*str != 0) {
+		hash = hash * 0x01000193;
+		hash ^= *str;
+		str++;
+	}
+
+	return hash;
+}
+
+int
+string_key_equals(const void *a, const void *b)
+{
+	return strcmp(a, b) == 0;
+}
